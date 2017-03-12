aba0d1fe2e6589a1605fd20bcf0db8824144edcf
Evgeniy Bezdomnikov <e.bezdomnikov@gmail.com>
2017-03-12 23:46:54 +0300

    task 1 first commit

---
+++ task1/structure.sql
@@ -0,0 +1,108 @@
+CREATE TABLE `category` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `enabled` varchar(1) NOT NULL DEFAULT 'Y',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `feed` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(150) NOT NULL,
+  `content` varchar(243) NOT NULL,
+  `enabled` varchar(1) NOT NULL DEFAULT 'Y',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `feed_category` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `category_id` int(11) unsigned NOT NULL,
+  `feed_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq_category_feed` (`category_id`,`feed_id`),
+  KEY `feed_id` (`feed_id`),
+  KEY `idx_category_id` (`category_id`), -- Need for Inner Join and Selects
+  CONSTRAINT `feed_category_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `feed_category_ibfk_2` FOREIGN KEY (`feed_id`) REFERENCES `feed` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `feed_user_like` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `feed_id` int(11) unsigned NOT NULL,
+  `islike` tinyint(1) DEFAULT NULL COMMENT '1- like, 2-unlike, null - no user desicion',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_id` (`user_id`,`feed_id`), -- uniq for insert 
+  KEY `idx_user_id` (`user_id`), -- Need for Inner Join and Selects
+  KEY `feed_id` (`feed_id`), -- Need for Inner Join and Selects
+  CONSTRAINT `feed_user_like_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `feed_user_like_ibfk_2` FOREIGN KEY (`feed_id`) REFERENCES `feed` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+
+
+
+-- Необходимо предусмотреть возможность просмотра списка всех оценивших пост пользователей.
+Select 
+user_id,feed_id,
+users.name,
+CASE feed_user_like.`islike` WHEN 1 THEN "LIKE" 
+WHEN 2 THEN "NOT LIKE" ELSE "" END islike
+from feed_user_like
+Inner Join users on users.id = feed_user_like.user_id
+Where feed_id IN (1,2) and IFNULL(feed_user_like.islike,0) > 0
+
+-- like (islike = 1 = LIKE)
+Insert Into feed_user_like (user_id,feed_id,islike) Values(3,1,1)
+ON DUPLICATE KEY UPDATE islike=1;
+
+-- unlike (islike = 2 = NOT LIKE)
+Insert Into feed_user_like (user_id,feed_id,islike) Values(3,1,2)
+ON DUPLICATE KEY UPDATE islike=2;
+
+
+-- Лента должна иметь фильтр по категориям. 
+Select Distinct
+	feed.id,feed.name,feed.content
+From feed
+Inner Join feed_category On feed_category.`feed_id` = feed.id
+Inner Join category On category.id = feed_category.category_id
+Where  category.id IN (1,3) -- filter by category Id
+And category.`enabled` = 'Y' -- only actived categories
+
+
+-- Лента
+select id, name from feed;
+
+-- Напишите запросы для выборки и обновления контента. Обоснуйте выбор индексов.
+-- update content on feed 
+update feed set content = "content" where id=1;
+-- link feed to category
+insert into feed_category (category_id, feed_id) values (1,1);
+-- unlink all feeds from specified category
+delete from feed_category where category_id = 1; -- IN (1,2);
+-- unlink specified feed(-s) from specified category(-ies)
+delete from feed_category where category_id = 1 and feed_id = 1;
+delete from feed_category where category_id IN (1,2) and feed_id IN (1,2);
+-- unlink specified feed from all category or categories 
+delete from feed_category where feed_id = 1 -- IN (1,2);
+
+-- new category
+insert into category (name) values ("new category name");
+-- update category
+update category set name = "updated category name" where id = 1;
+-- new feed
+insert into feed (name,content) values ("new feed name","new feed content");
+-- update feed
+update feed set name = "new feed name" where id = 1
+update feed set content = "new feed content" where id = 1 
+
+
+-- soft delete feed
+update feed set enabled = 'N'
+-- soft delete category
+update category set enabled = 'N'
+-- hard delete feed and links by FK
+delete from feed where id = 1;
+-- hard delete category and links by FK
+delete from category where id = 1;
+
